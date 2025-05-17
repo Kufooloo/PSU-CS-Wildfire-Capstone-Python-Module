03036b2c961a76cf03ad52462f945e3bad1e58ff
Jonah Wright <robot2boy@icloud.com>
2025-05-17 01:25:42 -0700

    Added intial test files and .gitignore

---
+++ test_wildfire.py
@@ -0,0 +1,4 @@
+import wildfire as wf
+
+wildfire_map = wf.wildfireMap()
+wildfire_map.helloWorld()
---
+++ wildfire.py
@@ -0,0 +1,10 @@
+import cartopy
+import xarray
+import datetime as dt
+
+
+class wildfireMap():
+    def __init__(self):
+        return None
+    def helloWorld(self):
+        print("Hello World")
